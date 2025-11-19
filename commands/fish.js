6ac957015b2b60eabcb6997fe939e9784485daa2
picklesonbread <martagrela62@gmail.com>
2025-11-19 22:21:25 +0100

    Add interaction.deferReply to fish command

--- commands/fish.js
+++ commands/fish.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { loadUserData, saveUserData } = require('../utils/storage');
 const database = require('../utils/database');
 
+await interaction.deferReply();
+
 // Fishing locations in Panfu
 const LOCATIONS = {
     beach: {
