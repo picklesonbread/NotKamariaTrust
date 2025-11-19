d9ce5af8e73c6be9c0e7c5964fb9556f9b0d9569
picklesonbread <martagrela62@gmail.com>
2025-11-19 22:21:08 +0100

    Update explore.js

--- commands/explore.js
+++ commands/explore.js
@@ -2,6 +2,8 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { loadUserData, saveUserData } = require('../utils/storage');
 const database = require('../utils/database');
 
+await interaction.deferReply();
+
 // Real Panfu locations
 const LOCATIONS = {
     beach: {
