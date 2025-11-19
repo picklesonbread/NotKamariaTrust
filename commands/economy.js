ec44d12fddadd98f21c45f18308b41c17ac3ad30
picklesonbread <martagrela62@gmail.com>
2025-11-19 22:20:36 +0100

    Add interaction deferReply to economy command

--- commands/economy.js
+++ commands/economy.js
@@ -10,6 +10,8 @@ module.exports = {
     async execute(message, args, client) {
         const userId = message.author.id;
         const userData = loadUserData();
+        
+await interaction.deferReply();
 
         // Initialize user data if doesn't exist
         if (!userData[userId]) {
